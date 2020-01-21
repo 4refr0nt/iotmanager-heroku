e27377e8eda9d021d9e7e7bc2fa263e1757832a4
4refr0nt <4refr0nt@gmail.com>
2020-01-21 19:07:20 +0300

    update

--- index.js
+++ index.js
@@ -156,6 +156,51 @@ config.push({
     order: 15,
 });
 
+config.push({
+  pageId : 3,
+  page   : 'input data',
+  widget : 'input',
+  topic  : prefix + "/" + deviceID + "/input_num",
+  descr  : "Input number",
+  size   : 'small',
+  color  : 'orange',
+  type   : 'number',
+  order  : 30,
+  status : 321
+};
+
+config.push({
+  pageId : 3,
+  widget : 'input',
+  topic  : prefix + "/" + deviceID + "/input_text",
+  descr  : "Input text",
+  type   : 'text',
+  order  : 40,
+  status : "some text"
+};
+
+config.push({
+  pageId : 3,
+  widget : 'input',
+  topic  : prefix + "/" + deviceID + "/input_time",
+  descr  : "Input time",
+  type   : 'time',
+  order  : 50,
+  status : "10:00"
+};
+
+config.push({
+  pageId : 3,
+  widget : 'input',
+  topic  : prefix + "/" + deviceID + "/input_date",
+  descr  : "Input date",
+  type   : 'date',
+  size   : 'small',
+  order  : 60,
+  status : "20.03.2020"
+};
+
+
 var client = mqtt.connect(opt);
 
 client.on('connect', function() {
@@ -203,7 +248,18 @@ client.on('message', function(topic, message) {
         setVentStatus();
         client.publish(prefix + "/esp32/venting/status", JSON.stringify(vent_status), { qos: 0 });
     }
-
+    if (topic.toString() === prefix + "/esp32/input_text/control") {
+        client.publish(prefix + "/esp32/input_text/status", JSON.stringify(status: message), { qos: 0 });
+    }
+    if (topic.toString() === prefix + "/esp32/input_num/control") {
+        client.publish(prefix + "/esp32/input_num/status", JSON.stringify(status: message), { qos: 0 });
+    }
+    if (topic.toString() === prefix + "/esp32/input_time/control") {
+        client.publish(prefix + "/esp32/input_time/status", JSON.stringify(status: message), { qos: 0 });
+    }
+    if (topic.toString() === prefix + "/esp32/input_date/control") {
+        client.publish(prefix + "/esp32/input_date/status", JSON.stringify(status: message), { qos: 0 });
+    }
 })
 
 function setAirStatus() {
